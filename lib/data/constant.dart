a41e0a6aac81884c9c75489115ef4fb3533b114b
Avinash Maurya <avinashmaurya@Mac.lan>
2026-03-13 21:18:07 +0530

    first commit

---
+++ lib/data/constant.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String baseUrl = 'https://fakestoreapi.com/';
+  static const String getAllProducts = 'products';
+  static const String getAllCartData = 'carts';
+}
